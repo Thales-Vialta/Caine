bba6232d4842897cb2425325a7c4258e11cd8385
Thales-Vialta <thalesvialta12@gmail.com>
2025-06-10 11:43:27 -0300

    0.1 Começando a pegar o endereço

---
+++ endereco.py
@@ -0,0 +1,35 @@
+'''Receber o CEP do usuário
+— No seu formulário, interface ou função, capture o CEP digitado.
+Consultar a API ViaCEP
+— Use sua função buscar_endereco() para pegar os dados do CEP (cidade, bairro, estado etc.).
+Verificar se o CEP já existe no banco
+— Antes de inserir, consulte o banco para ver se o CEP já está na tabela endereco. Se estiver, você não precisa inserir de novo.
+Se o CEP não existir:
+— Use os dados retornados pela API para inserir um novo registro na tabela endereco, incluindo o CEP, cidade, bairro e estado. Você pode pedir o número e descrição diretamente ao usuário nesse momento.
+Inserir a instituição ou entidade relacionada
+— Com o CEP já na tabela endereco, você pode inserir os dados da instituicao ou outra tabela que use esse CEP como chave estrangeira.
+Manter a integridade relacional
+— Garanta que, ao inserir dados em tabelas com chave estrangeira, os valores referenciados (como o CEP) já estejam no banco — esse processo resolve isso.
+Você pode colocar a inserção do endereço e da instituição em uma única função (ou módulo), que:
+
+chama a API,
+
+insere o endereço se for novo,
+
+e depois insere a instituição, usando o mesmo CEP como chave estrangeira.'''
+
+from database import conectar, criar_banco
+import requests
+def buscar_endereco(CEP):
+    url = f"https://viacep.com.br/ws/{CEP}/json/"
+    resposta = requests.get(url)
+def inserir_endereco(CEP,cidade,bairro,estado,numero,descricao):
+        con = conectar()
+        cursor = con.cursor()
+        cursor.execute(
+                "INSERT INTO endereco(CEP,cidade,bairro,estado,numero,descricao) VALUES (?,?,?,?,?,?)",
+                (CEP,cidade,bairro,estado,numero,descricao)
+        )
+        con.commit()
+        con.close()
+criar_banco()
